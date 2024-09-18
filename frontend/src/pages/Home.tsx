b3d15d7f09115880b030a75f67230700528a0cb8
pabloes <estornut@gmail.com>
2024-09-18 23:04:46 +0200

    x

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,6 +7,9 @@ const Home: React.FC = () => {
     return (
         <div className="uk-section uk-section-large uk-text-center">
             <div className="uk-container">
+                <h1 className="uk-heading-line"><span>SooN!</span></h1>
+            </div>
+      {/*      <div className="uk-container">
                 <h1 className="uk-heading-line"><span>Deploy your server in easy steps!</span></h1>
                 <p>With our service, you can deploy and publish your server, fully synced with your GitHub repository, in just a few clicks.</p>
                 {isLoggedIn ? (
@@ -14,7 +17,7 @@ const Home: React.FC = () => {
                 ) : (
                     <Link to="/register" className="uk-button uk-button-primary">Register for Free</Link>
                 )}
-            </div>
+            </div>*/}
         </div>
     );
 };
