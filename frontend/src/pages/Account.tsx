0c4562059017ab6177c4a17f4083ff2768e171b8
pabloes <estornut@gmail.com>
2024-09-26 17:41:46 +0200

    x

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -6,8 +6,6 @@ const AccountPage: React.FC = () => {
     const [changes, setChanges] = useState(0);
     return (
         <div className="uk-card uk-card-default uk-card-body">
-            <br/>
-            {changes}
             <AccountQuota changes={changes} />
             <br/>
             <BindWallet onAddWallet={()=>setChanges(changes+1)} onRemoveWallet={()=>setChanges(changes+1)} />
