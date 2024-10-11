04ca22b4b369e6e06cd31b9003bead37368d1c35
pabloes <estornut@gmail.com>
2024-10-11 11:36:30 +0200

    Add basename avatar image

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -2,22 +2,31 @@ import AccountQuota from "../components/AccountQuota";
 import BindWallet from "../components/BindWallet";
 import {useState} from "react";
 import {Link} from "react-router-dom";
+import {OnchainKitProvider} from "@coinbase/onchainkit";
+import {base} from "viem/chains";
+import { Avatar, Identity, Name, Badge, Address } from '@coinbase/onchainkit/identity';
+import {useAccount} from "wagmi";
+
 
 const AccountPage: React.FC = () => {
     const [changes, setChanges] = useState(0);
+    const { address, isConnected } = useAccount();
+
     return (
-        <div className="uk-card uk-card-default uk-card-body">
-            <div className="uk-section uk-container ">
-                <div className="uk-card uk-card-default uk-card-body">
-                    <Link to={"/my-articles"} >My articles</Link>&nbsp;|&nbsp;<Link to="/my-images">My images</Link>
+
+            <div className="uk-card uk-card-default uk-card-body">
+                <div className="uk-section uk-container ">
+                    <div className="uk-card uk-card-default uk-card-body">
+                        <Link to={"/my-articles"} >My articles</Link>&nbsp;|&nbsp;<Link to="/my-images">My images</Link>
+                    </div>
+
                 </div>
 
-            </div>
+                <AccountQuota changes={changes} />
+                <br/>
+                <BindWallet onAddWallet={()=>setChanges(changes+1)} onRemoveWallet={()=>setChanges(changes+1)} />
 
-            <AccountQuota changes={changes} />
-            <br/>
-            <BindWallet onAddWallet={()=>setChanges(changes+1)} onRemoveWallet={()=>setChanges(changes+1)} />
-        </div>
+            </div>
     );
 };
 
