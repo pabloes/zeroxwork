79bfeeaa561788e054282a30b7ad3a0c45dc6fb9
pabloes <estornut@gmail.com>
2024-10-13 22:14:57 +0200

    set base as chain to use

--- frontend/src/wagmi-config.ts
+++ frontend/src/wagmi-config.ts
@@ -1,5 +1,4 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains'
 import {base} from "viem/chains";
 export const wagmiConfig = createConfig({
     chains: [base],
